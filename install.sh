bd78ce697c729628c51cb5c1977a2791273a66da
gmusat <gmusat@artenea3d.com>
2019-02-02 17:50:13 +0100

    .

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+sudo apt install python3-pip -y
+
+sudo apt install python3-venv -y
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip3 install -r requirements.txt
+
+echo "
+#!/usr/bin/env bash
+source /home/pi/broker/venv/bin/activate
+cd /home/pi/broker
+python3 broker.py
+" > broker.sh
+
+sudo chmod +x broker.sh
+sudo mv broker.sh /bin/broker
+
+sudo touch /etc/systemd/system/broker.service
+sudo chmod 775 /etc/systemd/system/broker.service
+sudo chmod a+w /etc/systemd/system/broker.service
+
+sudo echo "
+[Unit]
+Description=broker
+[Service]
+User=pi
+ExecStart=/bin/bash /bin/broker
+Restart=on-failure
+WorkingDirectory=/home/pi/broker
+StandardOutput=syslog
+StandardError=syslog
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/broker.service
+
+sudo systemctl enable broker.service
+sudo systemctl daemon-reload
