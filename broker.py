a3f1dd892fa2c6d315d7443af975982ba4f7228b
gabriel <gabriel.musat@artenea3d.com>
2019-08-14 12:01:19 +0200

    connection ensure future added and removed task

--- broker.py
+++ broker.py
@@ -140,5 +140,5 @@ async def main():
 
 
 if __name__ == '__main__':
-    asyncio.create_task(connectToPrinter())
+    asyncio.ensure_future(connectToPrinter())
     asyncio.get_event_loop().run_until_complete(main())
